a17381f485b46711e3ac6d9bf32f34503f8b9538
kogiya <kie.ogiya@gmail.com>
2017-12-10 19:06:49 -0500

    Add Layout of LoginActivity, DashboadActivity and SignUpActivity

---
+++ app/src/main/java/team11/comp3074_project11/activity/DashboardActivity.java
@@ -0,0 +1,48 @@
+package team11.comp3074_project11.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import team11.comp3074_project11.R;
+
+/**
+ * Created by Owner on 12/9/2017.
+ */
+
+public class DashboardActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        //Get User id and name from the intent
+        Intent intent = getIntent();
+    }
+/*
+    //when click Search button, the page change to SearchActivity
+    public void onClickSearch(View v){
+        Intent intent = new Intent(DashboardActivity.this, SearchActivity.class);
+        startActivity(intent);
+    }
+
+    //when click Itineraries button, the page change to ItinerariesActivity
+    public void onClickItineraries(View v){
+        Intent intent = new Intent(DashboardActivity.this, ItinerariesActivity.class);
+        startActivity(intent);
+    }
+
+    //when click My Profile button, the page change to ProfileActivity
+    public void onClickProfile(View v){
+        Intent intent = new Intent(DashboardActivity.this, ProfileActivity.class);
+        startActivity(intent);
+    }
+*/
+    //when click Log Out button, the page change to LoginActivity
+    public void onClickLogOut(View v){
+        Intent intent = new Intent(DashboardActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
