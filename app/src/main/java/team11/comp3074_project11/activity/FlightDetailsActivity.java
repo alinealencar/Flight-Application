2e028a8514d2f6ad8f80824be2e8cca0aa275ffe
Aline <aline.nevesalencar@georgebrown.ca>
2017-12-14 10:24:02 -0500

    Modify listview to scrollview in results

---
+++ app/src/main/java/team11/comp3074_project11/activity/FlightDetailsActivity.java
@@ -0,0 +1,15 @@
+package team11.comp3074_project11.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import team11.comp3074_project11.R;
+
+public class FlightDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight_details);
+    }
+}
