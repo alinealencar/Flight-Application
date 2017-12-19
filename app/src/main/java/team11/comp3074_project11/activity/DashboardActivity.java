c5a2f6035a2fe462d164238c053cf127dca51ec0
Aline <aline.nevesalencar@georgebrown.ca>
2017-12-19 14:33:33 -0500

    Change loginactivity to oop

--- app/src/main/java/team11/comp3074_project11/activity/DashboardActivity.java
+++ app/src/main/java/team11/comp3074_project11/activity/DashboardActivity.java
@@ -8,15 +8,16 @@ import android.view.View;
 import android.widget.TextView;
 
 import team11.comp3074_project11.R;
+import team11.comp3074_project11.dataModel.Client;
+import team11.comp3074_project11.database.FlightAppDatabaseHelper;
+import team11.comp3074_project11.helper.SearchUtility;
 
 /**
  * Created by Owner on 12/9/2017.
  */
 
 public class DashboardActivity extends AppCompatActivity{
-    int intentClientId;
-    String intentClientFirstName;
-    String intentClientLastName;
+    static int intentClientId;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,12 +25,18 @@ public class DashboardActivity extends AppCompatActivity{
         setContentView(R.layout.activity_dashboard);
 
         Bundle extras = getIntent().getExtras();
-        //Get Client id and name from the intent
-        intentClientId = extras.getInt("clientId"); //client id
-        intentClientFirstName = extras.getString("firstName"); //client first name
-        intentClientLastName = extras.getString("lastName"); //client last name
 
-        String clientFullName = " " + intentClientFirstName + " " + intentClientLastName;
+        //Get Client id and name from the intent
+        if(Integer.valueOf(extras.getInt("clientId")) != null){
+            intentClientId = extras.getInt("clientId");
+        }
+
+        //Create client object
+        FlightAppDatabaseHelper db = new FlightAppDatabaseHelper(getApplicationContext());
+        Client client = SearchUtility.getClientByPK(db, intentClientId);
+
+        //Access properties of the object to compose the client's full name
+        String clientFullName = " " + client.getFirstName() + " " + client.getLastName();
 
         //display signed in user
         TextView clientView = (TextView)findViewById(R.id.txtClientName);
